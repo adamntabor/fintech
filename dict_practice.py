466b1ab49d00aad08168ad20237d01e0213f4a6a
deadego <87912314+deadego@users.noreply.github.com>
2021-12-11 23:19:55 -0800

    This python file defines a dictionary with four key value pairs related to loans.

---
+++ dict_practice.py
@@ -0,0 +1,9 @@
+#Creating a dictionary with bank names, maximum loan amounts, maximum debt-to-income ratios, and minimum FICO credit scores.
+banks = {
+	"Name": "HSBC",
+	"Maximum Loan Amount": 1000000,
+	"Maximum Debt-to-Income": 4.3,
+	"Minimum FICO Credit Score": 650
+}
+
+print(banks)
